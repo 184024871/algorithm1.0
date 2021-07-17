ce983c439a6bd421b07eac9d9f0eff6d327d6b68
ddliu <184024871@qq;com>
2021-07-17 23:09:23 +0800

    删除重复字符串

---
+++ src/main/test/com/iflytek/com/algorithm/DeleteRepeatString.java
@@ -0,0 +1,23 @@
+package com.iflytek.com.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeleteRepeatString {
+
+    public static void main(String[] args) {
+        String str = "aabbccddeefg";
+
+        char[] chars = str.toCharArray();
+        List list = new ArrayList(12);
+        StringBuffer charStr = new StringBuffer(12);
+        for (int i = 0; i < chars.length -1 ; i++) {
+            if (!list.contains(chars[i])){
+                list.add(chars[i]);
+                charStr.append(chars[i]);
+            }
+        }
+        System.out.println("Result:" + charStr);
+    }
+
+}
